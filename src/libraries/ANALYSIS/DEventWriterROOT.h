b56ec0f72ff1c19404ef06e5754be80c7425c2a2
Sean Dobbs <sdobbs@charm.physics.fsu.edu>
2019-02-24 20:58:20 -0500

    Add Daniel Lersch's updates for outputting pulls and error matrices in ROOT files.

--- src/libraries/ANALYSIS/DEventWriterROOT.h
+++ src/libraries/ANALYSIS/DEventWriterROOT.h
@@ -161,6 +161,76 @@ class DEventWriterROOT : public JObject
 				const DChargedTrackHypothesis* locChargedHypo, size_t locChargedIndex, DKinFitType locKinFitType) const;
 		void Fill_ComboNeutralData(DTreeFillData* locTreeFillData, unsigned int locComboIndex, string locParticleBranchName, const DNeutralParticleHypothesis* locMeasuredNeutralHypo,
 				const DNeutralParticleHypothesis* locNeutralHypo, size_t locNeutralIndex, DKinFitType locKinFitType) const;
+
+
+		//Decide whether to store pull-/error matrix- information:
+		mutable bool storePullInfo = false;
+		mutable bool storeErrMInfo = false; 
+		
+		void Create_KinFitBranches_DataTree(DTreeBranchRegister& locBranchRegister, JEventLoop* locEventLoop, const DReaction* locReaction, bool locIsMCDataFlag) const;
+		void Fill_KinFitBranches_DataTree(DTreeFillData* locTreeFillData, JEventLoop* locEventLoop, const DReaction* locReaction, const DMCReaction* locMCReaction, const vector<const DMCThrown*>& locMCThrowns,
+			const DMCThrownMatching* locMCThrownMatching, const DDetectorMatches* locDetectorMatches,
+			const vector<const DBeamPhoton*>& locBeamPhotons, const vector<const DChargedTrackHypothesis*>& locChargedHypos,
+			const vector<const DNeutralParticleHypothesis*>& locNeutralHypos, const deque<const DParticleCombo*>& locParticleCombos) const;
+
+		//Stuff to name and fill the branches correctly:
+		//Meaning, if is more than one particle of the same species (e.g. gamma),
+		//The naming will be consistent with the default naming in the default root-tree,
+		//e.g. Photon1__PullPx, Photon2__PullPx, and so on...
+		//The same holds for charged particles...
+		//****************************************************************************************************************
+		vector<string> getPIDinReaction(const DReaction* locReaction)const;
+		map<Particle_t,int> getNameForParticle(const DReaction* someReaction, vector<string> someVector)const;
+		mutable map<Particle_t,int> abundanceMap;
+		string assignName(Particle_t someParticle, map<Particle_t,int> someMap)const;
+		//****************************************************************************************************************
+
+		mutable map<const DReaction*, bool> writePulls;
+
+		//Lets try to get some pulls:
+		//****************************************************************************************************************
+		mutable map< const JObject*, map<DKinFitPullType, double> > myPullsMap;
+		void Get_PullsFromFit(const DParticleCombo* particleCombos)const;
+		map<DKinFitPullType, double> getPulls(const JObject* particle, const JObject* shower, DKinFitType yourFitType) const;
+		//****************************************************************************************************************
+
+		//Cosmetics to set and get the pulls-name/-type
+		//****************************************************************************************************************
+		//Set the tree branches.
+		void setTreePullBranches(DTreeBranchRegister& locBranchRegister,string yourBranchName,DKinFitType yourFitType, int yourNCombos, bool isNeutral) const;
+
+		//Fill the branches:
+		void fillTreePullBranches(DTreeFillData* locTreeFillData,string yourBranchName,DKinFitType yourFitType,map<DKinFitPullType, double> yourPullsMap, int yourIndex, bool isNeutral) const;
+		//****************************************************************************************************************
+
+		//Check, if pull-information is available (preserves crashes)
+		//****************************************************************************************************************
+		void setPullFlag(const DReaction* currentReaction, bool myFlag) const;
+		bool getPullFlag(const DReaction* currentReaction) const;
+		//****************************************************************************************************************
+
+		//Store the error martices for the kinematic fit:
+		//****************************************************************************************************************
+		int nEntriesParticleCov = 7+6+5+4+3+2+1; //--> (7X7) Particle Matrix is symmetric, therefore no need to look at all elements
+		int nEntriesShowerCov = 5+4+3+2+1; //--> (5X5) Shower Matrix is symmetric, therefore no need to look at all elements
+		int nEntriesDecayCov = 11+10+9+8+7+6+5+4+3+2+1; //--> (11x11) Decaying Particle Matrix is symmetric, therefore no need to look at all elements
+		//****************************************************************************************************************
+
+		//Save the elements of the error matrix to the tree: 
+		//****************************************************************************************************************
+		void fillTreeErrMBranches(DTreeFillData* locTreeFillData,string yourBranchName,DKinFitType yourFitType, const DKinematicData* particle, const DNeutralShower* shower, bool isNeutral) const;
+		//****************************************************************************************************************
+
+		//Fill tree for 7x7-Matrices:
+		//****************************************************************************************************************
+		void fillTreeParticleErrMBranches(DTreeFillData* locTreeFillData,string yourBranchName, const DKinematicData* particle) const;
+		//****************************************************************************************************************
+
+		 //Fill tree for 5x5-Matrices (showers):
+		//****************************************************************************************************************
+		void fillTreeShowerErrMBranches(DTreeFillData* locTreeFillData,string yourBranchName, const DNeutralShower* shower) const;
+		//****************************************************************************************************************
+
 };
 
 inline string DEventWriterROOT::Convert_ToBranchName(string locInputName) const
